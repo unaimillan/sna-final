0bffbe4c5945733b8eb8457657c62a90b31b47a6
Mike <super.mvk@yandex.ru>
2020-12-15 23:50:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# SNA Final webapp repository
+
+This repo hosts the Flask webapp and the Jenkins configuration for our SNA course
+final at IU
+
+## Setup
+
+Run the following line to install requirements
+
+> pip3 install -r requirements.txt
+
+and the following line to run the app
+
+> python3 app.py
+
---
+++ app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+from flask import Flask, render_template
+from redis import Redis
+
+app = Flask(__name__)
+redis = Redis(host="localhost", port=6379)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    counter = redis.incr("counter")
+    return render_template(
+        "index.html",
+        counter=counter
+    )
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=8000,
+        debug=True
+    )
