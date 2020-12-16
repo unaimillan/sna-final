5a6d257dbcb0190f859bfb8b55483b2e81eabe72
Mike <super.mvk@yandex.ru>
2020-12-16 22:50:08 +0300

    Update the real Redis ip

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host="localhost", port=6379)
+redis = Redis(host="10.0.10.14", port=6379)
 
 
 @app.route("/")
